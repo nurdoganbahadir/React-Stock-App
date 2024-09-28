3502ceb49a3962323617f121d5b27f272c6f816d
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-09-28 12:42:31 +0300

    formik oluşturuldu

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,37 +1,17 @@
-import Avatar from "@mui/material/Avatar"
-import Container from "@mui/material/Container"
-import Grid from "@mui/material/Grid"
-import Typography from "@mui/material/Typography"
-import LockIcon from "@mui/icons-material/Lock"
-import image from "../assets/result.svg"
-import { Link } from "react-router-dom"
-import Box from "@mui/material/Box"
-import TextField from "@mui/material/TextField"
-import { Button } from "@mui/material"
-import { Formik, Form } from "formik"
-import { object, string } from "yup"
-// import { login } from "../services/useApiRequest"
-import useApiRequest from "../services/useApiRequest"
+import Avatar from "@mui/material/Avatar";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import LockIcon from "@mui/icons-material/Lock";
+import image from "../assets/result.svg";
+import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+import { Formik, Form } from "formik";
 
 const Login = () => {
-  const { login } = useApiRequest()
-
-  const loginSchema = object({
-    email: string()
-      .email("Geçerli bir email giriniz")
-      .required("Email zorunludur"),
-    password: string()
-      .required("Şifre zorunludur")
-      .min(8, "Şifre en az 8 karakter olmalıdır")
-      .max(16, "Şifre en fazla 16 karakter olmalıdır")
-      .matches(/\d+/, "Şifre en az bir rakam içermelidir.")
-      .matches(/[a-z]+/, "Şifre en az bir küçük harf içermelidir.")
-      .matches(/[A-Z]+/, "Şifre en az bir büyük harf içermelidir.")
-      .matches(
-        /[@$!%*?&]+/,
-        "Şifre en az bir özel karakter(@$!%*?&) içermelidir."
-      ),
-  })
+  const loginSchema = {};
 
   return (
     <Container maxWidth="lg">
@@ -69,43 +49,26 @@ const Login = () => {
           >
             Login
           </Typography>
-
           <Formik
             initialValues={{ email: "", password: "" }}
             validationSchema={loginSchema}
             onSubmit={(values, actions) => {
-              //TODO
-              //? POST (Login)
-              //? Toastify
-              //? Global state güncellenmesi
-              //? form resetleme
-              //? navigate
-              login(values)
-              actions.resetForm()
-              actions.setSubmitting(false) //? isSubmitting
+              console.log(values);
             }}
           >
-            {({
-              values,
-              handleChange,
-              handleBlur,
-              touched,
-              errors,
-              isSubmitting,
-            }) => (
+            {() => (
               <Form>
-                <Box sx={{ display: "flex", flexDirection: "column", gap: 2 }}>
+                {" "}
+                <Box
+                  component="form"
+                  sx={{ display: "flex", flexDirection: "column", gap: 2 }}
+                >
                   <TextField
                     label="Email"
                     name="email"
                     id="email"
                     type="email"
                     variant="outlined"
-                    value={values.email}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    error={touched.email && Boolean(errors.email)}
-                    helperText={touched.email && errors.email}
                   />
                   <TextField
                     label="password"
@@ -113,17 +76,8 @@ const Login = () => {
                     id="password"
                     type="password"
                     variant="outlined"
-                    value={values.password}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    error={touched.password && Boolean(errors.password)}
-                    helperText={touched.password && errors.password}
                   />
-                  <Button
-                    variant="contained"
-                    type="submit"
-                    disabled={isSubmitting}
-                  >
+                  <Button variant="contained" type="submit">
                     Submit
                   </Button>
                 </Box>
@@ -143,7 +97,7 @@ const Login = () => {
         </Grid>
       </Grid>
     </Container>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
