9998311f7e8521841b1bc706589962b7228c6293
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-10-02 15:11:22 +0300

    modal eklendi modal içeriği değiştirilecek

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@mui/material";
+
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import * as React from "react";
 import Card from "@mui/material/Card";
@@ -8,14 +8,49 @@ import CardActions from "@mui/material/CardActions";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const Firm = () => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <>
       <h1>FIRMS</h1>
-      <Button variant="contained">
+      <Button onClick={handleOpen} variant="contained">
         <AddCircleOutlineIcon />
       </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
       <Card sx={{ maxWidth: 345 }}>
         <CardHeader
           title="Shrimp and Chorizo Paella"
