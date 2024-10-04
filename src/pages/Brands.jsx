bf38d7d14542c2a596d2d5238ee76c3c9baa966a
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-10-04 15:38:32 +0300

    brands sayfası ayarlanıyor grid yapısı oluşturuldu

--- src/pages/Brands.jsx
+++ src/pages/Brands.jsx
@@ -1,9 +1,42 @@
-
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Grid } from "@mui/material";
 
 const Brands = () => {
   return (
-    <div>Brands</div>
-  )
-}
+    <>
+      <h1>Brands</h1>
+      <Grid container spacing={1}>
+        <Grid item xs={6} md={4}>
+          <Card>
+            <CardMedia
+              sx={{ height: 140 }}
+              image="/static/images/cards/contemplative-reptile.jpg"
+              title="green iguana"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" sx={{ color: "text.secondary" }}>
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
 
-export default Brands
+export default Brands;
