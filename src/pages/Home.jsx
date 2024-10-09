7eaeb4c48bf53a4eb4f260392077ee704003061e
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-10-10 00:52:14 +0300

    home sayfasına başlandı

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,23 @@
+import React from "react";
+import KPICards from "../components/Dashboard/KPICards";
+import Charts from "../components/Dashboard/Charts";
+import { useEffect } from "react";
+import useStockRequests from "../services/useStockRequests";
+
 const Home = () => {
-  return <div>Home</div>;
+  const { getStock } = useStockRequests();
+
+  useEffect(() => {
+    getStock("purchases");
+    getStock("sales");
+  }, []);
+
+  return (
+    <div>
+      <KPICards />
+      <Charts />
+    </div>
+  );
 };
 
 export default Home;
