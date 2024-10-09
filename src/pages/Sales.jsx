4f8eb2cad1386d02a3056d66bc46ee3d02a5b76e
Nurdoğan Bahadır <nurdoganbahadirr@gmail.com>
2024-10-10 00:11:43 +0300

    sales sayfası hazırlandı

--- src/pages/Sales.jsx
+++ src/pages/Sales.jsx
@@ -1,9 +1,52 @@
-
+import { Button, Container } from "@mui/material";
+import { NoDataMessage, TableSkelthon } from "../components/Messages";
+import { useSelector } from "react-redux";
+import useStockRequests from "../services/useStockRequests";
+import { useEffect, useState } from "react";
+import SaleModal from "../components/Sales/SaleModal";
+import SaleTable from "../components/Sales/SaleTable";
 
 const Sales = () => {
-  return (
-    <div>Sales</div>
-  )
-}
+  const { getStock  } = useStockRequests();
+  const { sales, loading } = useSelector((state) => state.stock);
 
-export default Sales
+  const [open, setOpen] = useState(false);
+
+  const initialState = { brandId: "", productId: "", quantity: "", price: "" };
+  const [data, setData] = useState(initialState);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    setData(initialState);
+  };
+
+  useEffect(() => {
+    getStock("products");
+    getStock("sales");
+    getStock("brands");
+  }, []);
+  return (
+    <Container maxWidth="xl">
+      <Button variant="contained" onClick={handleOpen} sx={{ mb: 2 }}>
+        New Sale
+      </Button>
+
+      {loading && <TableSkelthon />}
+      {!loading && !sales?.length && <NoDataMessage />}
+
+      {!loading && sales?.length > 0 && (
+        <SaleTable setData={setData} handleOpen={handleOpen} />
+      )}
+
+      <SaleModal
+        open={open}
+        handleClose={handleClose}
+        data={data}
+        setData={setData}
+      />
+    </Container>
+  );
+};
+
+export default Sales;
